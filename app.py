6c44ee57371adbd6f302b11b0296218dac87945e
kvreddy422 <kvreddy422@gmail.com>
2018-11-27 23:38:11 -0700

    added REST calls for upvote downvote

--- app.py
+++ app.py
@@ -135,6 +135,32 @@ def create_sub_topics():
     return course_controller.create_sub_topics()
 
 
+@app.route('/upvote', methods=['POST'])
+def upvote_post():
+    user_obj = find_user(request.headers)
+    if user_obj == 403:
+        return Response(dumps({'status': 'Error unauthorized access'}), status=403)
+    posts = post_controller.upvote_post(request, user_obj)
+    return "Successfully updated"
+
+
+@app.route('/downvote', methods=['POST'])
+def downvote_post():
+    user_obj = find_user(request.headers)
+    if user_obj == 403:
+        return Response(dumps({'status': 'Error unauthorized access'}), status=403)
+    posts = post_controller.downvote_post(request, user_obj)
+    return "Successfully updated"
+
+
+@app.route('/get-posts', methods=['GET'])
+def get_posts():
+    user_obj = find_user(request.headers)
+    if user_obj == 403:
+        return Response(dumps({'status': 'Error unauthorized access'}), status=403)
+    return course_controller.get_sub_topic(request)
+
+
 @app.route('/create/sticky/note', methods= ['POST'])
 def create_sticky_note():
     user_obj = find_user(request.headers)
